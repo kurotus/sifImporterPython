140f8f2caddbaba7e52f9993f1e23be6cc67d929
Raja-Hanhela Toni <trajahan@ulapland.fi>
2016-06-17 12:32:18 +0300

    Comments

--- sifPython.py
+++ sifPython.py
@@ -3,6 +3,7 @@
 # This script reads a predefined CSV file and exports it as SIF 
 # We use this to import new students to our ILS
 # There is also a PHP version available, this python version is at the moment more primitive
+# 
 
 import csv
 import datetime
